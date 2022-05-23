780ba903f421d4689e0fa4f04fd1e68ef50423c6
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-05-23 22:12:37 +0300

    success!!!

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-const totalSpent = 4000;
+const totalSpent = 5550;
 let payment = 7000;
 let discount = 0;
-if () {
+if (totalSpent < 100) {
   discount = 0;
   console.log(`Not a partner, discount 0%`);
-} else if (totalSpent => 100 && totalSpent < 1000) {
+} else if (totalSpent < 1000) {
   discount = 2;
   payment = payment * 0.98;
   console.log(`Bronze partner, discount 2%`);
-} else if (totalSpent => 1000 && totalSpent < 5000) {
+} else if (totalSpent < 5000) {
   discount = 5;
   payment = payment * 0.95;
   console.log(`Silver partner, discount 5%`);
-} else (totalSpent => 5000) {
+} else {
   discount = 10;
   payment = payment * 0.9;
   console.log(`Golden partner, discount 10%`);
