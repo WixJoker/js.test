1e62932a0e9d1471499b660ea00885e7942bc810
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-06-10 16:46:00 +0300

    fghfcghgf

--- js/script.js
+++ js/script.js
@@ -1,2 +1,40 @@
-const numbers = [];
-console.log(numbers);
+// const countries = ['Ukraine', 'Poland', 'Croatia', 'Montenegro', 'France', 'USA'];
+// let availableCountries = [];
+// const countriesPrice = [100, 200, 300, 400, 500, 600];
+// let maxPrice = 400;
+// const userCredits = prompt('Enter available funds for your trip');
+// for (let i = 0; i < countries.length; i += 1) {
+//   console.log(countries[i], countriesPrice[i]);
+//   if (userCredits <= i) {
+//     availableCountries.push(i);
+//     console.log(availableCountries);
+//   }
+// }
+
+// let userName = '';
+// let userPass = '';
+// let userCountry = [];
+// let maxPrice = 0;
+
+// userName = prompt('Enter your login, please!');
+// if (userName === 'Andrii') {
+//   prompt('Hi Andrii! Enter your password!');
+// }
+// if (userPass === 'JavaScript') {
+//   console.log('Welcome, Andrii!');
+// } else {
+//   prompt('Wrong login! Enter your login once again, please!');
+// }
+
+// prompt('Wrong password! Enter once again!');
+
+// const userCredits = prompt('Enter available funds for your trip');
+// const availableCountries = function (countries, countriesPrice) {
+//   for (let i = 0; i < countriesPrice.length; i += 1) {
+//     console.log(countriesPrice[i], countries[i]);
+//     if (userCredits <= maxPrice) {
+//       userCountry.push(countries[userCountry]);
+//     }
+//     alert(`Available countries for you are ${countries[i]}!`);
+//   }
+// };
