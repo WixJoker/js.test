0bd2a53a73b931c42d704cf2922f84acdb5aa37a
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-07-08 17:21:57 +0300

    corrections

--- js/script.js
+++ js/script.js
@@ -1,30 +1,54 @@
-const DB = {
-  stuffing: {
-    STUFFING_SALAD: {
-      type: 'STUFFING_SALAD',
-      price: 300,
-      ccal: 200,
+class Hamburger {
+    static const DB = {
+        stuffing: {
+            STUFFING_SALAD: {
+                type: "STUFFING_SALAD",
+                price: 300,
+                ccal: 200,
+            },
+            STUFFING_POTATO: {
+                type: "STUFFING_POTATO",
+                price: 400,
+                ccal: 100,
+            },
+            TOPPING_MAYO: {
+                type: "TOPPING_MAYO",
+                price: 300,
+                ccal: 400,
+            },
+            TOPPING_SPICE: {
+                type: "TOPPING_SPICE",
+                price: 500,
+                ccal: 700,
+            },
+        },
+    }
+    sizes: {
+        S: 200,
+        L: 300,
+        M: 400,
     },
-    STUFFING_POTATO: {
-      type: 'STUFFING_POTATO',
-      price: 400,
-      ccal: 100,
-    },
-    TOPPING_MAYO: {
-      type: 'TOPPING_MAYO',
-      price: 300,
-      ccal: 400,
-    },
-    TOPPING_SPICE: {
-      type: 'TOPPING_SPICE',
-      price: 500,
-      ccal: 700,
-    },
-  },
+    addSize() {
+        console.log(Object.entries(this.sizes));
+    };
+    addTopping() {
 
-  sizes: {
-    S: 200,
-    L: 300,
-    M: 400,
-  },
-};
+    };
+    removeTopping() {
+
+    };
+    getStuffing() {
+        return this.toppings = []
+    };
+    calculatePrice() {
+
+    };
+    calculateCalories() {
+
+    };
+    showError() {
+        if () {
+
+        };
+    }
+}
--- js/script2.js
+++ js/script2.js
@@ -1,24 +1,24 @@
-const buttons = [
-  { label: 'Adams', color: '#42C325' },
-  { label: 'Boston', color: '#FA8A06' },
-  { label: 'Chicago', color: '#B9C325' },
-  { label: 'Denver', color: '#74AAF4' },
-  { label: 'Easy', color: '#E339D0' },
-  { label: 'Frank', color: '#E0214A' },
-];
+// const buttons = [
+//   { label: 'Adams', color: '#42C325' },
+//   { label: 'Boston', color: '#FA8A06' },
+//   { label: 'Chicago', color: '#B9C325' },
+//   { label: 'Denver', color: '#74AAF4' },
+//   { label: 'Easy', color: '#E339D0' },
+//   { label: 'Frank', color: '#E0214A' },
+// ];
 
-const btn = document.querySelector('body');
+// const btn = document.querySelector('body');
 
-const makeButtons = options => {
-  return options.map(({ color, label }) => {
-    const buttonRef = document.createElement('button');
-    buttonRef.type = 'button';
-    buttonRef.classList.add('hero-button');
-    buttonRef.style.color = color;
-    buttonRef.textContent = label;
+// const makeButtons = options => {
+//   return options.map(({ color, label }) => {
+//     const buttonRef = document.createElement('button');
+//     buttonRef.type = 'button';
+//     buttonRef.classList.add('hero-button');
+//     buttonRef.style.color = color;
+//     buttonRef.textContent = label;
 
-    return buttonRef;
-  });
-};
-const elements = makeButtons(buttons);
-btn.append(...elements);
+//     return buttonRef;
+//   });
+// };
+// const elements = makeButtons(buttons);
+// btn.append(...elements);
