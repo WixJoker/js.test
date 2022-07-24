ecc98453e30c6510b7dacdb439da9e270d750b75
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-07-24 22:35:41 +0300

    3rd task corrections

--- js/script3.js
+++ js/script3.js
@@ -1,52 +1,93 @@
+// import throttle from 'lodash.throttle';
+
+// const form = document.querySelector('.feedback-form');
+// const emailEl = document.querySelector('input');
+// const messageEl = document.querySelector('textarea');
+// const formData = {};
+
+// checkLocaleStorage();
+
+// emailEl.addEventListener('input', throttle(getInputValues, 500));
+// messageEl.addEventListener('input', throttle(getInputValues, 500));
+
+// function getInputValues(event) {
+//   event.preventDefault();
+
+//   const name = event.target.name;
+//   const value = event.target.value;
+
+//   formData[name] = value;
+
+//   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+// }
+
+// form.addEventListener('submit', sendFormData);
+
+// function sendFormData(event) {
+//   event.preventDefault();
+
+//   const email = event.target.elements.email.value;
+//   const message = event.target.elements.message.value;
+
+//   if (email === '' || message === '') {
+//     alert('Please fill in all the fields!');
+//   } else {
+//     localStorage.removeItem('feedback-form-state');
+//     console.log({ email, message });
+//   }
+
+//   event.currentTarget.reset();
+// }
+
+// function checkLocaleStorage() {
+//   const savedData = localStorage.getItem('feedback-form-state')
+//     ? JSON.parse(localStorage.getItem('feedback-form-state'))
+//     : {};
+//   if (savedData.email) {
+//     emailEl.value = savedData.email;
+//   }
+//   if (savedData.message) {
+//     messageEl.value = savedData.message;
+//   }
+// }
+
+
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-const emailEl = document.querySelector('input');
-const messageEl = document.querySelector('textarea');
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  email: document.querySelector('.feedback-form input[type=email]'),
+};
+const input = refs.form.elements;
+const FORM_STORAGE_KEY = 'feedback-form-state';
+const savedData = localStorage.getItem(FORM_STORAGE_KEY);
 const formData = {};
 
-checkLocaleStorage();
+refs.form.addEventListener('submit', onFormSubmit);
+refs.email.addEventListener('input', throttle(onInput, 500));
+refs.textarea.addEventListener('input', throttle(onInput, 500));
 
-emailEl.addEventListener('input', throttle(getInputValues, 500));
-messageEl.addEventListener('input', throttle(getInputValues, 500));
-
-function getInputValues(event) {
-  event.preventDefault();
-
-  const name = event.target.name;
-  const value = event.target.value;
-
-  formData[name] = value;
-
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+if (savedData) {
+  const formData = JSON.parse(savedData);
+  refs.email.value = formData.email;
+  refs.textarea.value = formData.message;
 }
 
-form.addEventListener('submit', sendFormData);
-
-function sendFormData(event) {
+function onInput(event) {
+  formData.email = input.email.value;
+  formData.message = input.message.value;
+  localStorage.setItem(FORM_STORAGE_KEY, JSON.stringify(formData));
+}
+function onFormSubmit(event) {
   event.preventDefault();
-
-  const email = event.target.elements.email.value;
-  const message = event.target.elements.message.value;
-
-  if (email === '' || message === '') {
-    alert('Please fill in all the fields!');
+  formData.email = input.email.value;
+  formData.message = input.message.value;
+  if (input.email.value === '' || input.message.value === '') {
+    alert('EMAIL and MESSAGE field can not be empty! Please fill in BOTH fields.');
   } else {
-    localStorage.removeItem('feedback-form-state');
-    console.log({ email, message });
-  }
-
-  event.currentTarget.reset();
-}
-
-function checkLocaleStorage() {
-  const savedData = localStorage.getItem('feedback-form-state')
-    ? JSON.parse(localStorage.getItem('feedback-form-state'))
-    : {};
-  if (savedData.email) {
-    emailEl.value = savedData.email;
-  }
-  if (savedData.message) {
-    messageEl.value = savedData.message;
+    console.log(formData);
+    refs.form.reset();
+    localStorage.removeItem(FORM_STORAGE_KEY);
   }
 }
