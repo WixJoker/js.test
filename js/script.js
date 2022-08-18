9b22220d5bffc6b0727f3190b9f107d0bdbc6356
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-08-18 16:25:53 +0300

    regular expresions

--- js/script.js
+++ js/script.js
@@ -1,29 +1,35 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
-const galleryItemsRef = document.querySelector('.gallery');
-
-function createGalleryItem({ preview, original, description }) {
-  return `<a class="gallery__item" href="${original}">
-      <img
-        class="gallery__image"
-        src="${preview}"
-        alt="${description}"
-      />
-    </a>`;
+import Throttle from 'lodash.throttle';
+const formRef = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+formRef.addEventListener('input', Throttle(handleFormInput), 500);
+populateFormOutput();
+const formData = {};
+function handleFormInput(event) {
+  const email = event.currentTarget.elements.email.value;
+  const message = event.currentTarget.elements.message.value;
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify({ email, message }));
 }
-function createGalleryList(galleryItems, createGalleryItem) {
-  return galleryItems.map(galleryItem => createGalleryItem(galleryItem)).join('');
+formRef.addEventListener('submit', handleFormSubmit);
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email.value;
+  const message = event.currentTarget.elements.message.value;
+  if (email === '' || message === '') {
+    return alert('Заполните все поля');
+  }
+  const saved = localStorage.getItem(STORAGE_KEY);
+  const parsed = JSON.parse(saved);
+  console.log(parsed);
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
 }
-
-galleryItemsRef.insertAdjacentHTML('beforeend', createGalleryList(galleryItems, createGalleryItem));
-
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-  captionPosition: 'bottom',
-});
-console.log(galleryItems);
+function populateFormOutput(event) {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  if (savedMessage) {
+    formRef.elements.email.value = JSON.parse(savedMessage).email;
+    formRef.elements.message.value = JSON.parse(savedMessage).message;
+    formData[event.target.name] = event.target.value;
+    formData = JSON.parse(savedMessage);
+  }
+}
