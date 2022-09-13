13810fb1f2636a6c6e3b096e5d1467fd0c0d7bbc
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-09-13 22:36:56 +0300

    test challenge

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
-import Throttle from 'lodash.throttle';
-const formRef = document.querySelector('.feedback-form');
-const STORAGE_KEY = 'feedback-form-state';
-formRef.addEventListener('input', Throttle(handleFormInput), 500);
-populateFormOutput();
-const formData = {};
-function handleFormInput(event) {
-  const email = event.currentTarget.elements.email.value;
-  const message = event.currentTarget.elements.message.value;
-  formData[event.target.name] = event.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify({ email, message }));
-}
-formRef.addEventListener('submit', handleFormSubmit);
-function handleFormSubmit(event) {
-  event.preventDefault();
-  const email = event.currentTarget.elements.email.value;
-  const message = event.currentTarget.elements.message.value;
-  if (email === '' || message === '') {
-    return alert('Заполните все поля');
-  }
-  const saved = localStorage.getItem(STORAGE_KEY);
-  const parsed = JSON.parse(saved);
-  console.log(parsed);
-  localStorage.removeItem(STORAGE_KEY);
-  event.currentTarget.reset();
-}
-function populateFormOutput(event) {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
-  if (savedMessage) {
-    formRef.elements.email.value = JSON.parse(savedMessage).email;
-    formRef.elements.message.value = JSON.parse(savedMessage).message;
-    formData[event.target.name] = event.target.value;
-    formData = JSON.parse(savedMessage);
-  }
-}
+// import Throttle from 'lodash.throttle';
+// const formRef = document.querySelector('.feedback-form');
+// const STORAGE_KEY = 'feedback-form-state';
+// formRef.addEventListener('input', Throttle(handleFormInput), 500);
+// populateFormOutput();
+// const formData = {};
+// function handleFormInput(event) {
+//   const email = event.currentTarget.elements.email.value;
+//   const message = event.currentTarget.elements.message.value;
+//   formData[event.target.name] = event.target.value;
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify({ email, message }));
+// }
+// formRef.addEventListener('submit', handleFormSubmit);
+// function handleFormSubmit(event) {
+//   event.preventDefault();
+//   const email = event.currentTarget.elements.email.value;
+//   const message = event.currentTarget.elements.message.value;
+//   if (email === '' || message === '') {
+//     return alert('Заполните все поля');
+//   }
+//   const saved = localStorage.getItem(STORAGE_KEY);
+//   const parsed = JSON.parse(saved);
+//   console.log(parsed);
+//   localStorage.removeItem(STORAGE_KEY);
+//   event.currentTarget.reset();
+// }
+// function populateFormOutput(event) {
+//   const savedMessage = localStorage.getItem(STORAGE_KEY);
+//   if (savedMessage) {
+//     formRef.elements.email.value = JSON.parse(savedMessage).email;
+//     formRef.elements.message.value = JSON.parse(savedMessage).message;
+//     formData[event.target.name] = event.target.value;
+//     formData = JSON.parse(savedMessage);
+//   }
+// }
--- js/script2.js
+++ js/script2.js
@@ -1,17 +1,17 @@
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
-const LOCALSTORAGE_KEY = 'videoplayer-current-time';
-const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+// const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
 
-if (currentTime) {
-  player.setCurrentTime(currentTime);
-}
-player.on(
-  'timeupdate',
-  throttle(data => {
-    localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
-  }, 1000)
-);
+// if (currentTime) {
+//   player.setCurrentTime(currentTime);
+// }
+// player.on(
+//   'timeupdate',
+//   throttle(data => {
+//     localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+//   }, 1000)
+// );
